e8dedba6dc22fa629570f442458cff0b6e553855
Louis Xu <louisxu2003@gmail.com>
2024-03-05 12:56:52 -0500

    trying api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,8 @@
-import { request } from "http";
 import { NextRequest, NextResponse } from "next/server";
 import { dbTest } from "@/app/lib/db";
 
 export async function GET(request: NextRequest) {
-  const data = await dbTest();
+  //   const data = await dbTest();
+  const data = { test: "test" };
   return NextResponse.json(data, { status: 200 });
 }

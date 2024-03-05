6cac0d7ddc110df56b6c321499d898de655bf876
Louis Xu <louisxu2003@gmail.com>
2024-03-05 18:53:33 -0500

    integrate db to deployment

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { dbTest } from "@/app/lib/db";
 
 export async function GET(request: NextRequest) {
-  //   const data = await dbTest();
-  const data = { test: "test" };
+  const data = await dbTest();
   return NextResponse.json(data, { status: 200 });
 }

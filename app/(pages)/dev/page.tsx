baaaab0a1ee89ecdacd1af271428ab3c41139b90
Louis Xu <louisxu2003@gmail.com>
2024-03-09 13:42:15 -0500

    deploy analytics

---
+++ app/(pages)/dev/page.tsx
@@ -0,0 +1,8 @@
+export default function DevPage() {
+  return (
+    <>
+      <div>DEV PAGE</div>
+      
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./_components/navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Outdoorboys Gear",

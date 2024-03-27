62ee9c5cbe259564facdc36b5d6688d7894bad94
Louis Xu <louisxu2003@gmail.com>
2024-03-27 02:32:47 -0400

    checking api endpoints

--- components/equipmentrow/index.tsx
+++ components/equipmentrow/index.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 
 export default function EquipmentRow(equipment: Equipment) {
   const media: Media = equipment.media;
+
   return (
     <div
       className="rounded-lg border bg-card text-card-foreground shadow-sm m-4 md:w-1/3 sm: w-full"
@@ -14,13 +15,11 @@ export default function EquipmentRow(equipment: Equipment) {
     >
       <div className="p-4 md:p-6">
         <div className="grid gap-4">
-          {/* <img
-            src={`${getApiURL()}${media.url}`}
-            alt="Cover"
-            width="400"
-            height="225"
-            className="aspect-video overflow-hidden rounded-lg object-cover"
-          /> */}
+          <div>
+            {getApiURL()}
+            {media.url}
+          </div>
+
           <Image
             src={`${getApiURL()}${media.url}`}
             alt="Cover"
--- components/foodrow/index.tsx
+++ components/foodrow/index.tsx
@@ -3,19 +3,5 @@ import { Food, Media } from "@/lib/interface";
 
 export default function FoodRow(food: Food) {
   const media: Media = food.media;
-  return (
-    <div className="flex items-center">
-      <img
-        className="w-10 h-10 mr-4"
-        src={`${getApiURL()}${media.url}`}
-        alt="Laptop"
-      />
-      <span className="flex flex-grow">
-        <span>{food.name}</span>
-        {/* generic */}
-        <span className="mx-1">| {food.description}</span>
-      </span>
-      <span className="text-gray-600">→</span>
-    </div>
-  );
+  return <div className="flex items-center">food</div>;
 }

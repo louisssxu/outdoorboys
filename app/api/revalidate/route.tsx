f28ac40f2dd81e18019ea4a3290a33177af37d14
Louis Xu <louisxu2003@gmail.com>
2024-03-30 19:48:08 -0400

    changed webhook listenner to post

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -1,20 +1,13 @@
 import { revalidatePath } from "next/cache";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get("path");
+export async function POST(request: NextRequest) {
   const token = request.nextUrl.searchParams.get("token");
 
   if (token !== process.env.CACHE_TOKEN) {
     return new Response("Unauthorized", { status: 401 });
   }
-  if (!path) {
-    return Response.json({
-      revalidated: false,
-      now: Date.now(),
-      message: "Missing path to revalidate",
-    });
-  }
+
   revalidatePath("/", "layout");
   return Response.json({ revalidated: true, now: Date.now() });
 }

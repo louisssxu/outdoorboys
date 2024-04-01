41f731a411f5d2d07504bc7135975a4f05bce9d3
Louis Xu <louisxu2003@gmail.com>
2024-04-01 14:04:21 -0400

    mobile view fix

--- app/(pages)/support/page.tsx
+++ app/(pages)/support/page.tsx
@@ -1,9 +1,8 @@
 export default function SupportPage() {
   return (
     <div className="container flex flex-col w-full content-center text-center min-h-svh">
-      <h1 className=" font-bold text-3xl m-20">
-        {" "}
-        Contact us @ outdoorboys.store@gmail.com
+      <h1 className=" font-bold md:text-3xl m-20 text-wrap text-2xl">
+        Contact us: outdoorboys.store@gmail.com
       </h1>
       <p className=" font-semibold text-muted-foreground">
         We are not affiliated with Luke, we are just fans of his content and we
